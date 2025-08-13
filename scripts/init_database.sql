f45842de80ce493ce92594fc502b02beb61569ad
Mohamed Abdellatef <160074503+MohamedAbdellatef@users.noreply.github.com>
2025-08-13 14:41:51 +0300

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,37 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouseProject' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+*/
+
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouseProject' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouseProject')
+BEGIN
+    ALTER DATABASE DataWarehouseProject SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouseProject;
+END;
+GO
+
+-- Create the 'DataWarehouseProject' database
+CREATE DATABASE DataWarehouseProject;
+GO
+
+USE DataWarehouseProject;
+GO
+
+-- Create Schemas for each layer in data archtecture
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
